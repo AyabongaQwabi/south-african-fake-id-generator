1cc0058458c708b15d08a6c31fe1bc205894c0bd
billyboy <aya@simply.co.za>
2020-08-05 17:25:05 +0200

    convert to es5

--- index.js
+++ index.js
@@ -1,66 +1,78 @@
-var saIdParser = require("south-african-id-parser");
-var moment =  require("moment");
+"use strict";
 
-const randomValueBetween = (min, max) => {
+var saIdParser = require("south-african-id-parser");
+
+var moment = require("moment");
+
+var randomValueBetween = function randomValueBetween(min, max) {
   return Math.random() * (max - min) + min;
 };
 
-const randomDate = (d1, d2) => {
-  let date1 = d1 || "01-01-1970";
-  let date2 = d2 || new Date();
+var randomDate = function randomDate(d1, d2) {
+  var date1 = d1 || "01-01-1970";
+  var date2 = d2 || new Date();
   date1 = new Date(date1).getTime();
   date2 = new Date(date2).getTime();
   return date1 > date2 ? new Date(randomValueBetween(date2, date1)) : new Date(randomValueBetween(date1, date2));
 };
 
-const getRandomSexDigits = () => Math.floor(1000 + Math.random() * 9000);
+var getRandomSexDigits = function getRandomSexDigits() {
+  return Math.floor(1000 + Math.random() * 9000);
+};
 
-const getFormatedRandomDate = (age) => {
-  const currentYear = new Date().getFullYear();
-  const yearAge = currentYear - age;
-  const ageYrBegin = `${yearAge}/01/01`;
-  const ageYrEnd = `${yearAge}/12/31`;
-  const randomdate = randomDate(ageYrBegin, ageYrEnd);
+var getFormatedRandomDate = function getFormatedRandomDate(age) {
+  var currentYear = new Date().getFullYear();
+  var yearAge = currentYear - age;
+  var ageYrBegin = "".concat(yearAge, "/01/01");
+  var ageYrEnd = "".concat(yearAge, "/12/31");
+  var randomdate = randomDate(ageYrBegin, ageYrEnd);
   return moment(randomdate).format("YYMMDD");
 };
 
-const getRandomCitizenshipDigit = () => randomValueBetween(0, 1);
-
-const getRandomZdigit = () => randomValueBetween(80, 89);
-
-const generateFakeId = (age) => {
-  const date = getFormatedRandomDate(age);
-  const sex = getRandomSexDigits();
-  const citizenship = Math.round(getRandomCitizenshipDigit());
-  const zDigit = Math.round(getRandomZdigit());
-  return `${date}${sex}${citizenship}${zDigit}`;
+var getRandomCitizenshipDigit = function getRandomCitizenshipDigit() {
+  return randomValueBetween(0, 1);
 };
 
-var generateFakeIdByAge = function(age) {
-    const fakeId = generateFakeId(age);
-    const isValid = saIdParser.validate(fakeId);
-    return isValid ? fakeId : generateFakeIdByAge(age);
-}
+var getRandomZdigit = function getRandomZdigit() {
+  return randomValueBetween(80, 89);
+};
+
+var _generateFakeId = function _generateFakeId(age) {
+  var date = getFormatedRandomDate(age);
+  var sex = getRandomSexDigits();
+  var citizenship = Math.round(getRandomCitizenshipDigit());
+  var zDigit = Math.round(getRandomZdigit());
+  return "".concat(date).concat(sex).concat(citizenship).concat(zDigit);
+};
+
+var generateFakeIdByAge = function generateFakeIdByAge(age) {
+  var fakeId = _generateFakeId(age);
+
+  var isValid = saIdParser.validate(fakeId);
+  return isValid ? fakeId : generateFakeIdByAge(age);
+};
 
 exports.generateFakeIdByAge = generateFakeIdByAge;
 
-var generateInvalidFakeIdByAge = function(age) {
-    const fakeId = generateFakeId(age);
-    const isValid = saIdParser.validate(fakeId);
-    return !isValid ? fakeId : generateInvalidFakeIdByAge(age);
-}
+var generateInvalidFakeIdByAge = function generateInvalidFakeIdByAge(age) {
+  var fakeId = _generateFakeId(age);
+
+  var isValid = saIdParser.validate(fakeId);
+  return !isValid ? fakeId : generateInvalidFakeIdByAge(age);
+};
+
 exports.generateInvalidFakeIdByAge = generateInvalidFakeIdByAge;
 
-exports.generateRandomId = function() {
-    const age = Math.floor(Math.random() * 90 + 10);
-    return generateFakeIdByAge(age);
-}
+exports.generateFakeId = function () {
+  var age = Math.floor(Math.random() * 90 + 10);
+  return generateFakeIdByAge(age);
+};
 
-exports.generateInvalidRandomId = function() {
-    const age = Math.floor(Math.random() * 90 + 10);
-    return generateInvalidFakeIdByAge(age);
-}
+exports.generateInvalidFakeId = function () {
+  var age = Math.floor(Math.random() * 90 + 10);
+  return generateInvalidFakeIdByAge(age);
+};
 
-exports.isValid = function(id) {
-    return saIdParser.validate(id);
-}
+exports.isValid = function (id) {
+  return saIdParser.validate(id);
+};
